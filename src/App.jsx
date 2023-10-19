a1ace6098e01b6a05db5ac04b148b8ac311eba2a
tanveer anjum <tanveerdar933@gmail.com>
2023-10-19 17:45:02 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Register from "./components/Register"
+
+function App() {
+  return (
+    <main className="app">
+      <Register />
+    </main>
+  )
+}
+
+export default App
---
+++ src/utils/regexs.js
@@ -0,0 +1,2 @@
+export const USER_REGEX = /^[a-zA-z][a-zA-Z0-9-_]{3,25}$/;
+export const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
